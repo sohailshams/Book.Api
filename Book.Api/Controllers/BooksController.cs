a64e8e1a0015bbdd8aecb4200e9ea4985bf73102
sohailshams <sohailshamas@yahoo.com>
2025-05-30 23:50:09 +0100

    Added a list of books and GetAllBook and GetBookById methods to fetch data.

---
+++ Book.Api/Controllers/BooksController.cs
@@ -0,0 +1,51 @@
+﻿using Book.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public static List<BookData> books = new List<BookData>
+        {
+            new BookData
+            {
+                Id = Guid.NewGuid(),
+                Title = "The Great Gatsby",
+                Author = "F. Scott Fitzgerald",
+                MyProperty = 1,
+                PublishedDate = new DateTime(1925, 4, 10)
+            },
+            new BookData
+            {
+                Id = Guid.NewGuid(),
+                Title = "1984",
+                Author = "George Orwell",
+                MyProperty = 2,
+                PublishedDate = new DateTime(1949, 6, 8)
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<BookData>> GetAllBooks()
+        {
+            return Ok(books);
+        }
+
+        [HttpGet]
+        [Route("{bookId:Guid}")]
+        public ActionResult<List<BookData>> GetBookById([FromRoute] Guid bookId)
+        {
+            var book = books.FirstOrDefault(b => b.Id == bookId);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+
+
+        }
+    }
+}
