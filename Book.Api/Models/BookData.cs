f360f803c28913d10f7d1d72c67a6ecbbaaf83e8
sohailshams <sohailshamas@yahoo.com>
2025-05-30 23:49:19 +0100

    Updated Book model to BookData.

---
+++ Book.Api/Models/BookData.cs
@@ -0,0 +1,11 @@
+﻿namespace Book.Api.Models
+{
+    public class BookData
+    {
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+        public int MyProperty { get; set; }
+        public DateTime PublishedDate { get; set; }
+    }
+}
