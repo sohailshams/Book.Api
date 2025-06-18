69f7e3e0ef33cf71d01aa7cb3f776eba81eb4bdf
sohailshams <sohailshamas@yahoo.com>
2025-06-18 22:51:33 +0100

    Created BookDbContext.

---
+++ Book.Api/EntityFramework/BookDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Book.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book.Api.EntityFramework
+{
+    public class BookDbContext(DbContextOptions<BookDbContext> Options) : DbContext(Options)
+    {
+        public DbSet<BookData> Books { get; set; }
+    }
+}
